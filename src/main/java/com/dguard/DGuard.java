fdbd310e9c89f0f78d80fb3b5ad00885c2c3d0fe
Duapar13 <bastien.rapaud@epitech.eu>
2026-07-08 10:16:23 +0200

    feat: classe principale DGuard - câblage
    Choix du backend de stockage selon config.yml, chargement des
    régions existantes, enregistrement de la commande /region, des 6
    listeners de protection, et publication de RegionService auprès de
    DAPI. Le plugin est fonctionnel de bout en bout.

---
+++ src/main/java/com/dguard/DGuard.java
@@ -0,0 +1,81 @@
+package com.dguard;
+
+import com.dguard.commands.RGCommand;
+import com.dguard.integration.DAPIHook;
+import com.dguard.listeners.EntryListener;
+import com.dguard.listeners.ExplosionListener;
+import com.dguard.listeners.FireListener;
+import com.dguard.listeners.MobSpawnListener;
+import com.dguard.listeners.ProtectionListener;
+import com.dguard.listeners.PvpListener;
+import com.dguard.manager.RegionManager;
+import com.dguard.storage.MySQLRegionStorage;
+import com.dguard.storage.RegionStorage;
+import com.dguard.storage.YamlRegionStorage;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DGuard extends JavaPlugin {
+
+    private RegionStorage storage;
+    private RegionManager regionManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        String storageType = getConfig().getString("storage.type", "local");
+        if ("mysql".equalsIgnoreCase(storageType)) {
+            storage = new MySQLRegionStorage(this,
+                    getConfig().getString("storage.mysql.host", "localhost"),
+                    getConfig().getInt("storage.mysql.port", 3306),
+                    getConfig().getString("storage.mysql.database", "dguard"),
+                    getConfig().getString("storage.mysql.username", "root"),
+                    getConfig().getString("storage.mysql.password", ""),
+                    getConfig().getBoolean("storage.mysql.useSSL", false));
+        } else {
+            storage = new YamlRegionStorage(getDataFolder(), getLogger());
+        }
+
+        try {
+            storage.init();
+        } catch (Exception e) {
+            getLogger().severe("Impossible d'initialiser le stockage (" + storageType + "): " + e.getMessage());
+            getLogger().severe("Le plugin va se désactiver.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        regionManager = new RegionManager(storage);
+        try {
+            regionManager.seed(storage.loadRegions());
+        } catch (Exception e) {
+            getLogger().severe("Erreur lors du chargement des régions existantes: " + e.getMessage());
+        }
+
+        RGCommand rgCommand = new RGCommand(this, regionManager);
+        PluginCommand command = getCommand("region");
+        if (command != null) {
+            command.setExecutor(rgCommand);
+            command.setTabCompleter(rgCommand);
+        }
+
+        getServer().getPluginManager().registerEvents(new ProtectionListener(regionManager), this);
+        getServer().getPluginManager().registerEvents(new ExplosionListener(regionManager), this);
+        getServer().getPluginManager().registerEvents(new MobSpawnListener(regionManager), this);
+        getServer().getPluginManager().registerEvents(new PvpListener(regionManager), this);
+        getServer().getPluginManager().registerEvents(new FireListener(regionManager), this);
+        getServer().getPluginManager().registerEvents(new EntryListener(regionManager), this);
+
+        DAPIHook.registerRegionService(this, regionManager);
+
+        getLogger().info("DGuard activé (stockage: " + storageType + ").");
+    }
+
+    @Override
+    public void onDisable() {
+        if (storage != null) {
+            storage.close();
+        }
+    }
+}
